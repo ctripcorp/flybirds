3ee18457993d75a04d404047151afc1ee2bbd431
clgwlg <chinachenliang@gmail.com>
2022-08-03 17:45:15 +0800

    chore:step name change

--- flybirds/core/dsl/globalization/i18n.py
+++ flybirds/core/dsl/globalization/i18n.py
@@ -89,8 +89,8 @@ step_language = {
         "screenshot": ["全屏截图"],
         "ocr": ["全屏扫描"],
         "change ocr lang [{param}]": ["切换OCR语言[{param}]"],
-        "image exist [{param}]": ["存在图像[{param}]"],
-        "image not exist [{param}]": ["不存在图像[{param}]"],
+        "exist image [{param}]": ["存在图像[{param}]"],
+        "not exist image [{param}]": ["不存在图像[{param}]"],
         "information association of failed operation, run the {param1} time"
         " :[{param2}]": ["失败运行的信息关联,运行第{param1}次:[{param2}]"],
         "text[{selector}]property[{param2}]is {param3}": [
