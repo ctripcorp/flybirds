729c429b1920828719120508759356cdbbfcc4bd
clgwlg <chinachenliang@gmail.com>
2022-11-25 17:29:20 +0800

    Bump version: 0.4.9 → 0.5.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ req = parse_requirements("requirements.txt")
 
 setup(
     name="flybirds",
-    version="0.4.9",
+    version="0.5.0",
     author="trip_flight",
     author_email="flybirds_support@trip.com",
     description="BDD-driven natural language automated testing framework",
