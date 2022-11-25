f0118169ef937b5a3c4d88c598fa4af862e0642f
clgwlg <chinachenliang@gmail.com>
2022-11-25 15:43:03 +0800

    feat:add keyboard_clear

--- flybirds/core/plugin/plugins/default/step/input.py
+++ flybirds/core/plugin/plugins/default/step/input.py
@@ -5,6 +5,8 @@ Step implement of element input.
 import time
 import flybirds.core.global_resource as gr
 import flybirds.utils.dsl_helper as dsl_helper
+from flybirds.core.global_resource import get_device_id
+from airtest.core.android.adb import *
 
 from flybirds.core.global_context import GlobalContext as g_Context
 from flybirds.core.plugin.plugins.default.step.click import click_ocr_text
@@ -54,6 +56,19 @@ def ocr_text_input(context, param1, param2):
     try:
         after_input_wait = gr.get_frame_config_value("after_input_wait", 1)
         time.sleep(after_input_wait)
+        keyboard_clear(context)
         g_Context.element.str_input(param2, after_input_wait)
     except Exception:
         raise Exception("[ocr_text_input] ocr input text error!")
+
+
+def keyboard_clear(context):
+    time.sleep(1)
+    dev = ADB()
+    device_id = get_device_id()
+    move_end = "-s {} shell input keyevent KEYCODE_MOVE_END".format(device_id)
+    long_del = "-s {} shell input keyevent --longpress 67 67 67 67 67 67 67 67 67 67 " \
+               "67 67 67 67 67 67 67 67 67 67 67".format(device_id)
+    dev.start_cmd(move_end, False)
+    dev.start_cmd(long_del, False)
+
