ee9ede0afff38e74f31b3e23340dcf007f4d023d
xu_zhou <xu_zhou@trip.com>
2023-03-20 15:30:48 +0800

    fix: code commit, delete gap

--- flybirds/core/plugin/plugins/default/web/page.py
+++ flybirds/core/plugin/plugins/default/web/page.py
@@ -184,7 +184,7 @@ class Page:
         schema_url_value = gr.get_page_schema_url(url_key)
 
         if "timeout" in param_dict.keys():
-            self.page.goto(schema_url_value, timeout = float(param_dict["timeout"]) * 1000)
+            self.page.goto(schema_url_value, timeout=float(param_dict["timeout"]) * 1000)
             return
 
         self.page.goto(schema_url_value)
