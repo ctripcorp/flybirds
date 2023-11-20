10ca863e7c283c63ff16b4ad43447669178e7b8f
xingwenyang <xingwenyang@trip.com>
2023-11-20 15:55:23 +0800

    fix: add null

--- flybirds/utils/verify_helper.py
+++ flybirds/utils/verify_helper.py
@@ -9,6 +9,8 @@ def text_equal(o_text, t_text):
     """
     Determine whether the text is equal
     """
+    if o_text == "[@@空@@]" or o_text == "@@空@@":
+        o_text = ""
     if o_text != t_text:
         message = "text not equal, expect value:{}, actual value:{}".format(
             o_text, t_text
@@ -40,6 +42,9 @@ def attr_equal(o_attr, t_attr):
     """
     Determine whether the attributes are equal
     """
+    if o_attr == "[@@空@@]" or o_attr == "@@空@@":
+        o_attr = ""
+
     if str(o_attr) != str(t_attr):
         message = "attr not equal, expect value:{}, actual value:{}".format(
             o_attr, t_attr
