58ab93733301b1b0bdea2b5e89a329e38e8bf181
yong.peng <yong.peng@trip.com>
2025-07-23 10:47:51 +0800

    feat:update version to 0.7.33

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ req = parse_requirements("requirements.txt")
 
 setup(
     name="flybirds",
-    version="0.7.32",
+    version="0.7.33",
     author="trip_flight",
     author_email="flybirds_support@trip.com",
     description="BDD-driven natural language automated testing framework",
